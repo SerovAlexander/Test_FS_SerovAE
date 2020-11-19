9fc7d9a84a38737107f738d1bfdcd883f2e819c5
Aleksandr Serov <Serov-ae@roxiemobile.com>
2020-11-19 11:06:21 +0300

    TestFS: Created project.

---
+++ Test_FS/Test_FS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_FS
+//
+//  Created by Серов Александр Евгеньевич on 19.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
